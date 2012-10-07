4c802e562372d15e5c9833bf2f045cbce36c5d5e
Ben <Ben@6b846a5b-ea6f-6648-92a3-f29226832327>
2012-10-07 11:12:04 +0000

    time to commit.

--- goodhead.php
+++ goodhead.php
@@ -12,7 +12,14 @@ jimport('joomla.plugin.plugin');
  */
 class plgSystemGoodHead extends JPlugin {
 
+    private $_currentURI = null;
+    private $_documentType = null;
+    private $_dublinCoreFlag = false;
     private $_fields = array();
+    private $_locale = null;
+    private $_metaDesc = null;
+    private $_openGraphFlag = false;
+    private $_siteName = null;
     protected $_head = null;
 
     /**
@@ -29,24 +36,116 @@ class plgSystemGoodHead extends JPlugin {
 
         $this->_plugin = JPluginHelper::getPlugin('system', 'goodhead');
         $this->_params = new JForm($this->_plugin->params);
+
+        // Set variables
+        $app = & JFactory::getApplication();
+        $doc = & JFactory::getDocument();
+        $this->_currentURI = JURI::current();
+        $this->_locale = $this->prepareLocaleTag($doc->getLanguage());
+        $this->_documentType = $doc->getType();
+        $this->_dublinCoreFlag = $this->params->get('dublincore_flag', 0);
+        $this->_metaDesc = $app->getCfg('MetaDesc');
+        $this->_openGraphFlag = $this->params->get('opengraph_flag', 0);
+        $this->_siteName = $app->getCfg('sitename');
+
+        // Collect the data
         $this->getFieldsFromXML();
     }
 
     /**
-     * Method to collect data from the parameters.
+     * Method to prepare and create Dublin Core tags.
+     * Reference: http://dublincore.org/documents/dc-html/
      *
      * @access  private
      * @return  void
      * @since   1.0
      */
-    private function collectData() {
-        foreach ($this->_fields as $field) {
-            $data[$field] = $this->params->get($field . '_field', '');
-        }
+    private function generateDublinCoreTags() {
 
-        foreach ($data as $name => $content) {
-            $this->_head .= $this->metaTag($name, $content);
+    }
+
+    /**
+     * Method to prepare and create <link> tags.
+     *
+     * @access  private
+     * @return  void
+     * @since   1.0
+     */
+    private function generateLinkTags() {
+        $this->_head .= $this->linkTag('canonical', $this->_currentURI);
+    }
+
+    /**
+     * Method to prepare and create Open Graph tags.
+     * Reference: http://ogp.me/
+     * Reference: https://developers.facebook.com/docs/opengraphprotocol/
+     *
+     * @access  private
+     * @return  void
+     * @since   1.0
+     */
+    private function generateOpenGraphTags() {
+        if ($_SERVER['REQUEST_URI'] == '/') {
+            // for the root of the domain only
+            $type = 'website';
+        } else {
+            $type = $this->params->get('opengraph_type', 'article');
         }
+        
+        // Set variables
+        $imagePath = $this->_currentURI . 'images/' . $this->params->get('image');
+        $imageData = getimagesize($imagePath);
+
+        // required
+        $this->_head .= $this->metaTag('og:url', $this->_currentURI, null, true);
+        $this->_head .= $this->metaTag('og:title', $this->getDocumentTitle(), null, true);
+        $this->_head .= $this->metaTag('og:type', $type, null, true);
+        $this->_head .= $this->metaTag('og:image:url', $imagePath, null, true);
+        $this->_head .= $this->metaTag('og:image:type', $imageData['mime'], null, true);
+        $this->_head .= $this->metaTag('og:image:height', $imageData[1], null, true);
+        $this->_head .= $this->metaTag('og:image:width', $imageData[0], null, true);
+        // FUTURE FEATURE: Support og:image:secure_url
+
+        // optional
+        $this->_head .= $this->metaTag('og:audio', $this->params->get('audio'), null, true);
+        $this->_head .= $this->metaTag('og:description', $this->_metaDesc, null, true);
+        // The word that appears before this object's title in a sentence.
+        // An enum of (a, an, the, "", auto). If auto is chosen, the consumer
+        // of your data should chose between "a" or "an". Default is "" (blank).
+        // $this->_head .= $this->metaTag('og:determiner', null, null, true);
+        $this->_head .= $this->metaTag('og:locale', $this->_locale, null, true);
+        // FUTURE FEATURE: detect available langauge versions of the current page, and list them here
+        // $this->_head .= $this->metaTag('og:locale:alternate', $this->_otherLocales, null, true);
+        $this->_head .= $this->metaTag('og:site_name', $this->_siteName, null, true);
+        $this->_head .= $this->metaTag('og:video', $this->params->get('video'), null, true);
+
+        // other
+        $this->_head .= $this->metaTag('og:email', $this->params->get('email'), null, true);
+        $this->_head .= $this->metaTag('og:phone_number', $this->params->get('phone'), null, true);
+        $this->_head .= $this->metaTag('og:fax_number', $this->params->get('fax'), null, true);
+        // To associate the page with your Facebook account, add the additional
+        // property fb:admins to your page with a comma-separated list of the
+        // user IDs or usernames of the Facebook accounts who own the page.
+        // See: https://developers.facebook.com/docs/opengraphprotocol/
+        // $this->_head .= $this->metaTag('fb:admins', null, null, true);
+        $this->_head .= $this->metaTag('og:latitude', $this->params->get('latitude'), null, true);
+        $this->_head .= $this->metaTag('og:longitude', $this->params->get('longitude'), null, true);
+        $this->_head .= $this->metaTag('og:street-address', $this->params->get('address'), null, true);
+        $this->_head .= $this->metaTag('og:locality', $this->params->get('city'), null, true);
+        $this->_head .= $this->metaTag('og:region', $this->params->get('state'), null, true);
+        $this->_head .= $this->metaTag('og:postal-code', $this->params->get('zip'), null, true);
+        $this->_head .= $this->metaTag('og:country-name', $this->params->get('country'), null, true);
+    }
+
+    /**
+     * Method to retrieve the document title.
+     *
+     * @access  private
+     * @return  string  Document title
+     * @since   1.0
+     */
+    private function getDocumentTitle() {
+        return JFactory::getDocument()->getTitle();
     }
 
     /**
@@ -92,16 +191,45 @@ class plgSystemGoodHead extends JPlugin {
     }
 
     /**
-     * Method to create the meta tags.
+     * Method to create link tags.
      *
      * @access  private
-     * @param   string  Name for the meta tag
-     * @param   string  Content for the meta tag
+     * @param   string  Rel attribute for the link tag
+     * @param   string  Href attribute for the link tag
+     * @return  string  Link tag
+     * @since   1.0
+     */
+    private function linkTag($rel, $href) {
+        return '<link rel="' . $rel . '" href="' . $href . '" />' . "\n  ";
+    }
+
+    /**
+     * Method to create meta tags.
+     *
+     * @access  private
+     * @param   string  Name attribute for the meta tag
+     * @param   string  Content attribute for the meta tag
+     * @param   string  Scheme attribute for the meta tag
+     * @param   boolean Flag used for OpenGraph meta tags. changes "name" attribute to "property"
      * @return  string  Meta tag
      * @since   1.0
      */
-    private function metaTag($name, $content) {
-        return '<meta name="' . $name . '" content="' . $content . '" />' . "\n  ";
+    private function metaTag($name, $content, $scheme = null, $openGraph = false) {
+        if (empty($content)) {
+            return;
+        }
+
+        if ($openGraph === true) {
+            $attribute = 'property';
+        } else {
+            $attribute = 'name';
+        }
+
+        if (!empty($scheme)) {
+            $scheme = ' scheme="' . $scheme . '"';
+        }
+
+        return '<meta ' . $attribute . '="' . $name . '" content="' . $content . '"' . $scheme . ' />' . "\n  ";
     }
 
     /**
@@ -113,6 +241,7 @@ class plgSystemGoodHead extends JPlugin {
      */
     public function onAfterRender() {
         // Set the variables
+        $buffer = JResponse::getBody();
         $input = JFactory::getApplication()->input;
 
         // Use this plugin only in site application, and check if we should insert data into the html header
@@ -120,16 +249,77 @@ class plgSystemGoodHead extends JPlugin {
             return true;
         }
 
-        // Collect the data
-        $this->collectData();
+        // Prepare the data.
+        $this->generateLinkTags();
+
+        if ($this->_openGraphFlag) {
+            // Add the xmlns:og to the <html> tag
+            $patterns = array(chr(1) . '(<html.*)(>)' . chr(1) . 'i');
+            $replace = array('${1}' . ' xmlns:og="http://ogp.me/ns#"' . '${2}');
+            $buffer = preg_replace($patterns, $replace, $buffer);
+
+            $this->generateOpenGraphTags();
+        }
+
+        if ($this->_dublinCoreFlag) {
+            // Add the profile attribute to the <head> tag
+            $patterns = array(chr(1) . '(<head.*)(>)' . chr(1) . 'i');
+            $replace = array('${1}' . ' profile="http://dublincore.org/documents/2008/08/04/dc-html/"' . '${2}');
+            $buffer = preg_replace($patterns, $replace, $buffer);
+
+            $this->generateDublinCoreTags();
+        }
+        #$this->prepareData();
+        // Get custom meta data
+        $other = $this->params->get('other');
+        // TODO: SANITIZE $other
+        $this->_head .= $other;
 
         // Adjust the buffer.
-        $buffer = JResponse::getBody();
         $pos = strrpos($buffer, "<title>");
+        // FUTURE FEATURE: Double-check that there aren't any duplicate tags.
         $buffer = substr($buffer, 0, $pos) . $this->_head . substr($buffer, $pos);
         JResponse::setBody($buffer);
 
         return true;
+    }
+
+    /**
+     * Method to prepare data for injection.
+     *
+     * @access  public
+     * @return  void
+     * @since   1.0
+     */
+    private function prepareData() {
+        foreach ($this->_fields as $field) {
+            $data[$field] = $this->params->get($field . '_field', '');
+        }
+
+        foreach ($data as $name => $content) {
+            $this->_head .= $this->metaTag($name, $content);
+        }
+    }
+
+    /**
+     * Method to prepare data for injection.
+     *
+     * @access  public
+     * @return  void
+     * @since   1.0
+     */
+    private function prepareLocaleTag($before) {
+        $parts = explode('-', $before);
+
+        if (strtolower($parts[1]) == 'gb' && strtolower(substr($this->params->get('country'), 2)) == 'us') {
+            $parts[1] = 'US';
+        } else {
+            $parts[1] = strtoupper($parts[1]);
+        }
+
+        $after = implode('_', $parts);
+
+        return $after;
     }
 
 }
